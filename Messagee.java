680b65fea3e7643f8caed0c06e5404c12cfddc8c
melika <94879109+melika9180@users.noreply.github.com>
2023-02-14 22:53:05 +0300

    Add files via upload

---
+++ Messagee.java
@@ -0,0 +1,15 @@
+package com.sxillocc.androidchat;
+
+public class Messagee {
+    public String email;
+    public String message;
+
+    public Messagee(){
+        //Required
+    }
+
+    public Messagee(String email,String message){
+        this.email = email;
+        this.message = message;
+    }
+}
